d4670d150de5bf6dc12b30915bff598d1d31f137
Tiago Catoia <tiagocsz@hotmail.com>
2025-06-25 18:14:57 -0300

    feat: update maincontroller to use firebase dao and coroutines

--- app/src/main/java/com/example/personaltasks/controller/MainController.kt
+++ app/src/main/java/com/example/personaltasks/controller/MainController.kt
@@ -1,34 +1,51 @@
 package com.example.personaltasks.controller
 
+import android.os.Message
 import androidx.room.Room
+import com.example.personaltasks.model.Constant.EXTRA_TASK_ARRAY
 import com.example.personaltasks.model.Task
 import com.example.personaltasks.model.TaskDao
+import com.example.personaltasks.model.TaskDatabaseFirebase
 import com.example.personaltasks.model.TaskRoomDb
 import com.example.personaltasks.ui.MainActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class MainController(mainActivity: MainActivity) {
-    private val taskDao: TaskDao = Room.databaseBuilder(
-        mainActivity, TaskRoomDb::class.java, "task-database"
-    ).fallbackToDestructiveMigration(true).build().taskDao()
+class MainController(private val mainActivity: MainActivity) {
+    // Implementação com RoomDb
+//    private val taskDao: TaskDao = Room.databaseBuilder(
+//        mainActivity, TaskRoomDb::class.java, "task-database"
+//    ).fallbackToDestructiveMigration(true).build().taskDao()
+    private val taskDao: TaskDao = TaskDatabaseFirebase()
+    private val databaseCoroutineScope = CoroutineScope(Dispatchers.IO)
 
     fun insertTask(task: Task) {
-        Thread{
+        databaseCoroutineScope.launch {
             taskDao.createTask(task)
-        }.start()
+        }
     }
 
-    // Necessário chamar esses métodos fora da Thread principal para não travar a UI
     fun getTask(id: Int) = taskDao.retrieveTask(id)
-    fun getAllTasks() = taskDao.retrieveTasks()
+
+    fun getAllTasks() {
+        databaseCoroutineScope.launch {
+            val tasks = taskDao.retrieveTasks()
+            mainActivity.getTasksHandler.sendMessage(Message().apply {
+                data.putParcelableArray(EXTRA_TASK_ARRAY, tasks.toTypedArray())
+            })
+        }
+    }
 
     fun modifyTask(task: Task) {
-        Thread{
+        databaseCoroutineScope.launch {
             taskDao.updateTask(task)
-        }.start()
+        }
     }
+
     fun removeTask(task: Task) {
-        Thread {
+        databaseCoroutineScope.launch {
             taskDao.deleteTask(task)
-        }.start()
+        }
     }
 }
