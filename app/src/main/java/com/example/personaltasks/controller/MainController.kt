b5fdf5c514191fa7d2d39a1befa9f1c57298983f
Tiago Catoia <tiagocsz@hotmail.com>
2025-06-27 08:11:46 -0300

    chore: remove unused dependencies

--- app/src/main/java/com/example/personaltasks/controller/MainController.kt
+++ app/src/main/java/com/example/personaltasks/controller/MainController.kt
@@ -1,12 +1,10 @@
 package com.example.personaltasks.controller
 
 import android.os.Message
-import androidx.room.Room
 import com.example.personaltasks.model.Constant.EXTRA_TASK_ARRAY
 import com.example.personaltasks.model.Task
 import com.example.personaltasks.model.TaskDao
 import com.example.personaltasks.model.TaskDatabaseFirebase
-import com.example.personaltasks.model.TaskRoomDb
 import com.example.personaltasks.ui.MainActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
